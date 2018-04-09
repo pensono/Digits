1c6f425c4604bc68b80dcda40d9447ae6f7a7743
Ethan Shea <ethan.shea1@gmail.com>
2018-04-08 20:46:44 -0700

    Enhance ErrorInput's properties

--- app/src/main/java/com/ethshea/digits/ErrorInput.kt
+++ app/src/main/java/com/ethshea/digits/ErrorInput.kt
@@ -30,27 +30,34 @@ class ErrorInput: EditText {
     var underlinePaint: Paint = Paint()
     var rect : Rect = Rect()
 
+    var underlineOffset: Float = 0.0f
+    var underlineColor: Int
+        get() = underlinePaint.color
+        set(value) { underlinePaint.color = value }
+    var underlineThickness: Float
+        get() = underlinePaint.strokeWidth
+        set(value) { underlinePaint.strokeWidth = value }
+
     private fun init(context: Context?, attributeSet: AttributeSet?, defStyle: Int) {
         val density = context!!.resources.displayMetrics.density
 
         val typedArray = context.obtainStyledAttributes(attributeSet, R.styleable.ErrorInput, defStyle, 0)
-        val underlineColor = typedArray.getColor(R.styleable.ErrorInput_underlineColor, -0x10000)
-        val underlineThickness = typedArray.getDimension(R.styleable.ErrorInput_underlineThickness, density * 2)
+        underlineColor = typedArray.getColor(R.styleable.ErrorInput_underlineColor, -0x10000)
+        underlineThickness = typedArray.getDimension(R.styleable.ErrorInput_underlineThickness, density * 2)
+        underlineOffset = typedArray.getDimension(R.styleable.ErrorInput_underlineThickness, underlineThickness)
         typedArray.recycle()
 
         underlinePaint.style = Paint.Style.STROKE
-        underlinePaint.color = underlineColor
-        underlinePaint.strokeWidth = underlineThickness
     }
 
     override fun onDraw(canvas: Canvas) {
-        val baseline = getLineBounds(0, rect).toFloat()
+        val underlineY = getLineBounds(0, rect).toFloat() + underlineOffset
         val left = layout.getLineLeft(0)
 
         for (error in errors) {
             val start = paint.measureText(text, 0, error.position)
             val end = paint.measureText(text, 0, error.position + 1)
-            canvas.drawLine(left + start, baseline, left + end, baseline, underlinePaint)
+            canvas.drawLine(left + start, underlineY, left + end, underlineY, underlinePaint)
         }
 
         System.out.println(errors)
