b66cd570ee3358c4bb8d703f86ae85fc7f1c463d
Ethan Shea <ethan.shea1@gmail.com>
2020-12-26 23:28:30 -0800

    Remove usages of deprecated code

--- app/src/main/java/com/monotonic/digits/SettingsActivity.kt
+++ app/src/main/java/com/monotonic/digits/SettingsActivity.kt
@@ -1,17 +1,17 @@
 package com.monotonic.digits
 
-import android.app.Activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 /**
+ * For some reason, the official settings document uses appcompat. https://developer.android.com/guide/topics/ui/settings/
  * @author Ethan
  */
-class SettingsActivity : Activity() {
+class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         actionBar?.setDisplayHomeAsUpEnabled(true) // Show back button
-        fragmentManager
+        supportFragmentManager
                 .beginTransaction()
                 .replace(android.R.id.content, SettingsFragment())
                 .commit()
--- app/src/main/java/com/monotonic/digits/SettingsFragment.kt
+++ app/src/main/java/com/monotonic/digits/SettingsFragment.kt
@@ -1,17 +1,15 @@
 package com.monotonic.digits
 
 import android.os.Bundle
-import android.preference.PreferenceFragment
-
+import androidx.preference.PreferenceFragmentCompat
 
 
 /**
  * @author Ethan
  */
 
-class SettingsFragment : PreferenceFragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        addPreferencesFromResource(R.xml.settings)
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings, rootKey)
     }
 }
